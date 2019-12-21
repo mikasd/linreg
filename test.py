98f627ab12b36b27e210df2fab2b1f4130e4b138
mikasd <michael.ingley@gmail.com>
2019-12-21 14:02:14 -0800

    setup linear regression along with basic filtering to optimize model accuracy, and matplotlib vis

--- test.py
+++ test.py
@@ -19,16 +19,28 @@ predict = "G3"
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x,y, test_size = 0.1)
 
-linear = linear_model.LinearRegression()
+# best = 0
+# for _ in range(30):
+#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x,y, test_size = 0.1)
 
-linear.fit(x_train, y_train)
+#     linear = linear_model.LinearRegression()
 
-acc = linear.score(x_test, y_test)
+#     linear.fit(x_train, y_train)
 
-print(acc)
+#     acc = linear.score(x_test, y_test)
+
+#     print(acc)
+
+#     if acc > best:
+#         best = acc
+#         with open("studentmodel.pickle", "wb") as f:
+#             pickle.dump(linear, f)
+
+pickle_in = open("studentmodel.pickle", "rb")
+
+linear = pickle.load(pickle_in)
 
 print("Co: ", linear.coef_)
 print("Intercept ", linear.intercept_)
@@ -37,6 +49,13 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+p = "G2"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
 
 
 
